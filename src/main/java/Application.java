f55736d3fe51e15b7bd6eb7ce35588a4ac24fa37
VyacheslavYakimchuk <vyacheslav.yakimchuk@gmail.com>
2014-12-28 02:05:55 +0200

    player object was renamed

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,8 +3,8 @@
  */
 public class Application {
     public static void main(String[] args) {
-        UserDialog player = new UserDialog(args[0]);
-        player.start();
+        UserDialog userDialog = new UserDialog(args[0]);
+        userDialog.start();
 
     }
 
